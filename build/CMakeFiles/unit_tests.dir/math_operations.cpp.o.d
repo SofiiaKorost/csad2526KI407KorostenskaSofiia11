6bd97141c08dea9015dbb766ca18fdd0a5695316
Sofiia Korostenska <korostenska@icloud.com>
2025-10-14 16:34:27 +0300

    Fix: remove macOS Icon file causing Windows checkout failure

---
+++ build/CMakeFiles/unit_tests.dir/math_operations.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/unit_tests.dir/math_operations.cpp.o: \
+  /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/math_operations.cpp \
+  /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/math_operations.h
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11
+# Build directory: /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/build/unit_tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/CMakeLists.txt;16;add_test;/Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/CMakeLists.txt;0;")
