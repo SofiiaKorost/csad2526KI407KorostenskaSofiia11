3507a519aa24bb742d3f5d8dbd3b5133d10fcc13
Sofiia Korostenska <korostenska@icloud.com>
2025-10-16 19:29:48 +0300

    Remove build directory from repo and add to .gitignore

--- build/CMakeFiles/unit_tests.dir/math_operations.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/unit_tests.dir/math_operations.cpp.o: \
-  /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/math_operations.cpp \
-  /Users/mac/Desktop/csad2526KI407KorostenskaSofiia11/math_operations.h
